1d81f96b6e02f5bc5f45e9613c0706aeac45215d
bexruz-cyber <mayoq2024@gmail.com>
2025-01-01 17:17:22 +0500

    add

--- js/Service.js
+++ js/Service.js
@@ -1,5 +1,16 @@
+let hmgr = document.querySelector(".header-hmgr");
+let exit = document.querySelector(".exit-btn");
+let hamburger_bar = document.querySelector(".hamburger-bar");
+let body = document.querySelector("body");
 
-
+hmgr.addEventListener("click", function () {
+  hamburger_bar.style.left = "0";
+  body.style.overflowY = "hidden";
+});
+exit.addEventListener("click", function () {
+  hamburger_bar.style.left = "-100%";
+  body.style.overflowY = "scroll";
+});
 
 
 let btn = document.querySelector(".light");
